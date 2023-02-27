a83ba4ee3a8e334b5b51433143a9a20480c0f699
Reno <101154856+RenoRenoRenoReno@users.noreply.github.com>
2023-02-26 20:15:33 -0500

    Create source.lua

---
+++ source.lua
@@ -0,0 +1,21 @@
+if (game.PlaceId == 5938036553) then
+	game.Players.LocalPlayer:Kick("This game does not support univerasl ESP./ ")
+end
+
+for _,Player in pairs(game.Players.LocalPlayer.Character.Parent) do
+	if (Player:FindFirstChild("Humanoid")) then
+		local highlight = Instance.new("Highlight", Player)
+		print("added ESP to "..Player.Name)
+		if (_G.CheckTeams == true) then
+		    if (game.Players.LocalPlayer.Team ~= game.Players[Player.name].Team) then
+			    highlight.FillColor = _G.EnemyColor
+		    else
+		        highlight.FillColor = _G.TeamColor	
+		    end
+		end
+		if (Player.Name == game.Players.LocalPlayer.Name and game.Players.LocalPlayer.Character:FindFirstChild("Highlight")) then
+			Player.Highlight:Destroy()
+			print("Removed ESP from player")
+		end
+	end
+end
