0206daafbd744d94399cfc976094c164673cd96a
Reno <101154856+RenoRenoRenoReno@users.noreply.github.com>
2023-02-26 20:23:07 -0500

    Update example.lua

--- example.lua
+++ example.lua
@@ -1,5 +1,5 @@
 _G.CheckTeams = true
 _G.EnemyColor = Color3.fromRGB(255,0,0)
-_G.TeamColor =  Color3.fromRGB(111 0 255)
+_G.TeamColor =  Color3.fromRGB(111, 0, 255)
 
 loadstring(game:HttpGet('https://raw.githubusercontent.com/RenoRenoRenoReno/UniversalESP/Main/source.lua'))()
